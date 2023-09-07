efde9e28120147cbd7a0bbed6ed403d3fb05848b
alina513 <130853290+alina513@users.noreply.github.com>
2023-09-07 15:36:42 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+start.addEventListener("click", onClickStart);
+stop.addEventListener("click", onClickStop);
+let timerId = null;
+
+
+function onClickStart() {
+start.disabled = true;
+timerId = setInterval(() => {body.style.backgroundColor = getRandomHexColor()}, 1000); 
+ return timerId};
+
+function onClickStop() {
+    clearInterval(timerId);
+    start.disabled = false;
+
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+
 
